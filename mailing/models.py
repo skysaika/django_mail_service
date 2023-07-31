fd4df257fa69d8cf47afe95d67be7ebd60b53108
saysuu <skysaika@yandex.ru>
2023-07-31 19:01:54 +0300

    create model MessagesLogs

--- mailing/models.py
+++ mailing/models.py
@@ -91,3 +91,36 @@ class MailingSettings(models.Model):
         verbose_name = 'Настройка рассылки'
         verbose_name_plural = 'Настройки рассылок'
         ordering = ('sending_status',)
+
+
+class MessagesLogs(models.Model):
+    """
+    Модель логов рассылки
+    mailing_settings: настройки рассылки
+    last_attempt_time: время последней попытки
+    attempt_status: статус попытки
+    server_response: ответ сервера
+    """
+
+    ATTEMPT_STATUS_CHOICES = [
+        (None, 'Не указано'),
+        ('received', 'Получено'),
+        ('failed', 'Провалено')
+    ]
+
+    mailing_settings = models.ForeignKey('MailingSettings', on_delete=models.CASCADE, verbose_name='Настройки рассылки')
+
+    last_attempt_time = models.DateTimeField(default=timezone.now, verbose_name='Время последней попытки')
+
+    attempt_status = models.CharField(max_length=8, choices=ATTEMPT_STATUS_CHOICES, verbose_name='Статус попытки',
+                                      default=None)
+
+    server_response = models.CharField(max_length=100, **NULLABLE, verbose_name='Ответ сервера')
+
+    def __str__(self):
+        return f'Статус попытки: {self.attempt_status}'
+
+    class Meta:
+        verbose_name = 'Логи рассылки'
+        verbose_name_plural = 'Логи рассылок'
+        ordering = ('attempt_status',)
