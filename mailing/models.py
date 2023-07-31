e520af69b9dc93ae3c97244ecec0ab2406db5d5b
saysuu <skysaika@yandex.ru>
2023-07-31 18:58:45 +0300

    create model MailingSettings

--- mailing/models.py
+++ mailing/models.py
@@ -39,3 +39,55 @@ class Customer(models.Model):
         ordering = ('pk',)
 
 
+class MailingSettings(models.Model):
+    """
+    Модель настроек рассылки
+    customers: клиенты
+    send_time: время рассылки
+    frequency: периодичность
+    sending_status: статус отправки
+    message_title: тема письма
+    message_content: содержание
+    mailing_owner: создатель рассылки
+    """
+    STATUS_CHOICES = [
+        (None, 'Не указано'),
+        ('created', 'Создана'),
+        ('active', 'Запущена'),
+        ('closed', 'Завершена')
+    ]
+
+    FREQUENCY_CHOICES = [
+        (None, 'Не указано'),
+        ('OPD', 'Раз в день'),
+        ('OPW', 'Раз в неделю'),
+        ('OPM', 'Раз в месяц')
+    ]
+
+    customers = models.ManyToManyField('Customer', verbose_name='Клиенты')
+
+    # Атрибуты для настройки рассылки
+    send_time = models.DateTimeField(default=timezone.now, verbose_name='Время рассылки')
+
+    frequency = models.CharField(
+        max_length=3, choices=FREQUENCY_CHOICES, default=None, verbose_name='Периодичность'
+    )
+
+    sending_status = models.CharField(
+        max_length=7, choices=STATUS_CHOICES, default=None, verbose_name='Статус отправки'
+    )
+
+    # Атрибуты для создания письма в рассылке
+    message_title = models.CharField(max_length=250, verbose_name='Тема письма')
+    message_content = models.TextField(verbose_name='Содержание')
+
+    # Атрибут для связи создатель-рассылка
+    mailing_owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Создатель рассылки')
+
+    def __str__(self):
+        return f'Периодичность: {self.frequency}. Статус отправки: {self.sending_status}'
+
+    class Meta:
+        verbose_name = 'Настройка рассылки'
+        verbose_name_plural = 'Настройки рассылок'
+        ordering = ('sending_status',)
